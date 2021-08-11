ad56e15ff6cf95ff0bc831f76829cb78d8fe738c
antaresofficial <maria.b.2021.06@gmail.com>
2021-08-11 23:55:54 +0500

    init project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || config.get("port") || 3000;
+const app = express();
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () =>
+      console.log(`Server has been started on port ${PORT} `)
+    );
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
