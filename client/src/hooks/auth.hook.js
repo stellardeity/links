5e53f4a8eec7f6498a910b1ebe32321abe81088d
antaresofficial <maria.b.2021.06@gmail.com>
2021-08-16 03:21:43 +0500

    auth hook

---
+++ client/src/hooks/auth.hook.js
@@ -0,0 +1,34 @@
+import { useState, useCallback, useEffect } from "react";
+
+const storageName = "userData";
+
+export const useAuth = () => {
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+
+  const login = useCallback(
+    (jwtToken, id) => {
+      setToken(jwtToken);
+      setUserId(id);
+
+      localStorage.setItem(storageName, JSON.stringify(userId, token));
+    },
+    [token, userId]
+  );
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    localStorage.removeItem(storageName);
+  }, []);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem(storageName));
+
+    if (data && data.token) {
+      login(data.token, data.userId);
+    }
+  });
+
+  return { login, logout, token, userId };
+};
--- client/src/hooks/message.hook.js
+++ client/src/hooks/message.hook.js
@@ -2,7 +2,6 @@ import { useCallback } from "react";
 
 export const useMessage = () => {
   return useCallback((text) => {
-    // console.log(text);
     if (text) {
       window.alert(text);
     }
