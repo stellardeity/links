8fef52364e18f07f4f4530db5f0eaad705cd8c20
antaresofficial <maria.b.2021.06@gmail.com>
2021-08-16 17:44:43 +0500

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import { Container } from "@material-ui/core";
 import { useRoutes } from "./routes";
+import { useAuth } from "./hooks/auth.hook";
+import { AuthContext } from "./context/auth.context";
 
 const App = () => {
-  const routes = useRoutes(false);
-  return <Container>{routes}</Container>;
+  const { token, login, logout, userId } = useAuth();
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        login,
+        logout,
+        userId,
+        isAuthenticated,
+      }}
+    >
+      <Container>{routes}</Container>
+    </AuthContext.Provider>
+  );
 };
 
 export default App;
